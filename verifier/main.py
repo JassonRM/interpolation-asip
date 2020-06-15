3cc755349ba7d8ccad0ae42462a8d89d039daf9b
Jasson Rodriguez <jassonrm@icloud.com>
2020-06-15 15:28:51 -0600

    Add decryption verifier python

---
+++ verifier/main.py
@@ -0,0 +1,77 @@
+import numpy
+import cv2
+import math
+
+
+def main():
+    with open("data.txt", "r") as input:
+        data = input.readlines()
+
+    imageHeight = 640
+    imageWidth = 480
+
+    encrypted = []
+    for line in data:
+        encrypted += line.split()
+
+    print(encrypted.__len__())
+
+    encrypted_image = numpy.array(encrypted, dtype=numpy.uint8)
+    encrypted_image.shape = (2 * imageHeight, imageWidth)
+    cv2.imshow("Encrypted", encrypted_image)
+
+    encrypted2byte = []
+    for i in range(0, encrypted.__len__(), 2):
+        byte1 = int(encrypted[i])
+        byte2 = int(encrypted[i + 1])
+        pixel = 256 * byte1 + byte2
+        encrypted2byte.append(pixel)
+
+    # Esto tiene que hacerlo el hardware
+    d = 1441
+    n = 3763
+
+    decrypted = []
+    for pixel in encrypted2byte:
+        decrypted.append(modular_pow(pixel, d, n))
+
+    decrypted_image = numpy.array(decrypted, dtype=numpy.uint8)
+    decrypted_image.shape = (imageHeight, imageWidth)
+    cv2.imshow("Decrypted", decrypted_image)
+    cv2.waitKey()
+
+
+def modular_pow(c, d, n):
+    print("Should output: ", pow(c, d, n))
+    if n == 1:
+        return 0
+    c = vector_modulo(c, n)
+    result = 1
+    while d > 0:
+        if (d % 2) == 1:
+            result = result * c  # Vector multiplication
+            result = vector_modulo(result, n)  # Vectorized modulo
+        d = d // 2
+        c = c * c  # Vector multiplication
+        c = vector_modulo(c, n)  # Vectorized modulo
+    print("Outputs: ", result)
+    return result
+
+
+def vector_modulo(a, n):
+    # Solo se debe hacer una vez para cada n, es decir una vez
+    k = math.log(n, 2)
+    m = 2 ** k // n
+
+    # Parte vectorizable
+    q = a * m  # Vector multiplication
+    q = q // 2 ** k  # Vector shift right
+    b = q * n  # Vector multiplication
+    a = a - b  # Vector subtraction
+    if n <= a:  # Vector subtraction with flags
+        a = a - n  # Conditional vector subtraction
+    return a
+
+
+if __name__ == "__main__":
+    main()
