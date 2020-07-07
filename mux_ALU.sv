ed9c9f2c9ce08e32bc952dbe347691945f5816f9
Edjchg <edjchg@gmail.com>
2020-07-07 00:04:30 -0600

    Or -> Sright

--- mux_ALU.sv
+++ mux_ALU.sv
@@ -1,14 +1,14 @@
 module mux_ALU (input logic [2:0]ALUop,
-					 input logic [31:0] add, sub, mul, sleft, and_, or_, 
+					 input logic [31:0] add, sub, mul, sleft, and_, sright, 
 					 output logic [31:0]selected);
-					 always @(ALUop, add,sub, mul, sleft, and_, or_) begin
+					 always @(ALUop, add,sub, mul, sleft, and_, sright) begin
 						case (ALUop)
 							3'b000 : selected = add;
 							3'b001 : selected = sub;
 							3'b010 : selected = mul;
 							3'b100 : selected = sleft;
 							3'b101 : selected = and_;
-							3'b110 : selected = or_;
+							3'b110 : selected = sright;
 							default: selected = 32'b0;
 						endcase				 
 					 end
