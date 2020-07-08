b920e062f037fe63b4177bb6a012eccd842a67f6
Edjchg <edjchg@gmail.com>
2020-07-08 14:37:55 -0600

    A correction over mult bits operands order.
    It receives 32 bit operands, but only take the first 16 bits of each operand to calculate.

--- processor/ALU/mul.sv
+++ processor/ALU/mul.sv
@@ -1,6 +1,6 @@
-module mul(input logic [15:0] OpA, OpB,
+module mul(input logic [31:0] OpA, OpB,
 				output logic [31:0] result);
 				
-				assign result = OpA*OpB;
+				assign result = OpA[15:0]*OpB[15:0];
 				
 endmodule
