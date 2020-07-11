32df969aa5cad09d790a2dda11b51c06a7bcf8ca
Edjchg <edjchg@gmail.com>
2020-07-10 18:47:01 -0600

    Use this module as the top vectorial register.
    It already contains the decode logic for the vectorial reg.

---
+++ processor/registers/top_vectorial_reg.sv
@@ -0,0 +1,13 @@
+module top_vectorial_reg(input logic WriteEn,clk,
+					          input logic [4:0] Rs1, Rs2,rd,
+								 input logic [255:0]InputData,
+								 output logic [255:0]Rout1, Rout2);
+								 
+								 logic [4:0] rs1_dec, rs2_dec, rd_dec;
+								 
+								 vectorialRegAddress decoAddress (Rs1,Rs2,rd, rs1_dec, rs2_dec, rd_dec);
+								 
+								 vectorial_registers VReg (WriteEn,clk, rs1_dec, rs2_dec, rd_dec,InputData,Rout1, Rout2);
+								 
+
+endmodule
