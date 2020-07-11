1651b32e32b5a9948d9eec420d69eddb0849952f
Edjchg <edjchg@gmail.com>
2020-07-10 18:44:13 -0600

    8 256 bit vectorial registers.

---
+++ processor/registers/vectorial_registers.sv
@@ -0,0 +1,16 @@
+module vectorial_registers (input logic WriteEn,clk,
+					          input logic [4:0] Rs1, Rs2,rd,
+								 input logic [255:0]InputData,
+								 output logic [255:0]Rout1, Rout2);
+								 
+								 logic [255:0] vectorial_reg[7:0];
+								 
+								 always_ff @(posedge clk) 
+										if (WriteEn) vectorial_reg[rd] <= InputData;
+								
+								assign Rout1 = vectorial_reg[Rs1];
+								assign Rout2 = vectorial_reg[Rs2];
+								
+									
+									
+endmodule
