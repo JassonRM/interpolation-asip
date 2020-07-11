ca3802da2bdc3875f7130005c89b7769299776b2
Edjchg <edjchg@gmail.com>
2020-07-11 15:41:04 -0600

    Change in WriteReg signal. 2 bits -> 1 bit.
    Small simplification.

--- processor/control/Control_Unit.sv
+++ processor/control/Control_Unit.sv
@@ -3,7 +3,7 @@ module Control_Unit (input logic [4:0] OPcode,
 							output logic [1:0] JMPSel,
 							output logic WriteRegister,
 							output logic MemWrite,
-							output logic [1:0]RegWrite,
+							output logic RegWrite,
 							output logic [1:0] vcsub,
 							output logic [2:0] ALUOp,
 							output logic [1:0] SelectorOpB,
@@ -19,38 +19,38 @@ module Control_Unit (input logic [4:0] OPcode,
 							assign {JMPSel, WriteRegister, MemWrite, RegWrite, vcsub, ALUOp, SelectorOpB, SelectorRs2, BranchSel,SelectorOpA,SelWriteData,WriteRegisterVec} = out_put;
 							
 							always @(*) begin
-								case ({OPcode,ALUop})       //  1 234 56  7 89 ABC  -> Those numbers match with the Data order specified before.
+								case ({OPcode,ALUop})       //  1 23456  7 89 ABC  -> Those numbers match with the Data order specified before.
 								   
-									8'b00000000 : out_put <= 18'b001001000000000000; // Control signals for ADD.
-									8'b01000000 : out_put <= 18'b001001000001000000; // Control signals for ADD Inmediate.
-									8'b00000001 : out_put <= 18'b001001000100000000; // Control signals for SUB.
-									8'b01001001 : out_put <= 18'b001001000101000000; // Control signals for SUB Inmediate.
-									8'b00000010 : out_put <= 18'b001001001000000000; // Control signals for MUL.
-									8'b00000100 : out_put <= 18'b001001010000000000; // Control signals for Shift Left.
-									8'b00000101 : out_put <= 18'b001001010100000000; // Control signals for AND.
-									8'b00000110 : out_put <= 18'b001001011000000000; // Control signals for Shift Right.
+									8'b00000000 : out_put <= 17'b00101000000000000; // Control signals for ADD.
+									8'b01000000 : out_put <= 17'b00101000001000000; // Control signals for ADD Inmediate.
+									8'b00000001 : out_put <= 17'b00101000100000000; // Control signals for SUB.
+									8'b01001000 : out_put <= 17'b00101000101000000; // Control signals for SUB Inmediate.
+									8'b00000010 : out_put <= 17'b00101001000000000; // Control signals for MUL.
+									8'b00000100 : out_put <= 17'b00101010000000000; // Control signals for Shift Left.
+									8'b00000101 : out_put <= 17'b00101010100000000; // Control signals for AND.
+									8'b00000110 : out_put <= 17'b00101011000000000; // Control signals for Shift Right.
 									
-									8'b00010000 : out_put <= 18'b001000000010100000; // Control signals for LoadWord.
-									8'b00011000 : out_put <= 18'b001000000010100010; // Control signals for LoadHalfWord.
-									8'b00100000 : out_put <= 18'b0001xx000010000100; // Control signals for StoreWord.
-									8'b00101000 : out_put <= 18'b0001xx000010000101; // Control signals for StoreHalfWord.
+									8'b00010000 : out_put <= 17'b00100000010100000; // Control signals for LoadWord.
+									8'b00011000 : out_put <= 17'b00100000010100010; // Control signals for LoadHalfWord.
+									8'b00100000 : out_put <= 17'b0001x000010000100; // Control signals for StoreWord.
+									8'b00101000 : out_put <= 17'b0001x000010000101; // Control signals for StoreHalfWord.
 									
-									8'b10000000 : out_put <= 18'b01xxxxxxxxxxx00xxx; // Control signals for JMP.
-									8'b00110000 : out_put <= 18'b10xxxxxxxxxxx00xxx; // Control signals for JR.
-									8'b10001000 : out_put <= 18'b00xxxxxxxxxxx00xxx; // Control signals for CALL (Check it after).
-									8'b01010000 : out_put <= 18'b01xxxxx0010001000x; // Control signals for BE.
-									8'b01011000 : out_put <= 18'b10xxxxx0010001000x; // Control signals for BGT.
+									8'b10000000 : out_put <= 17'b01xxxxxxxxxx00xxx; // Control signals for JMP.
+									8'b00110000 : out_put <= 17'b10xxxxxxxxxx00xxx; // Control signals for JR.
+									8'b10001000 : out_put <= 17'b00xxxxxxxxxx00xxx; // Control signals for CALL (Check it after).
+									8'b01010000 : out_put <= 17'b01xxxx0010001000x; // Control signals for BE.
+									8'b01011000 : out_put <= 17'b10xxxx0010001000x; // Control signals for BGT.
 									
-									8'b11000010 : out_put <= 18'b0000100010xxx00xx1; // Control signals for VMUL.
-									8'b11000111 : out_put <= 18'b0000100111xxx00xx1; // Control signals for VSR.
-									8'b11000101 : out_put <= 18'b0000100101xxx00xx1; // Control signals for VSUB.
-									8'b11110000	: out_put <= 18'b0000101101xxx00xx1; // Control signals for VCSUB.
-									8'b11010000	: out_put <= 18'b0000100000xxx00xx1; // Control signals for VLDW.
-									8'b11011000 : out_put <= 18'b0000100000xxx00xx1; // Control signals for VLDH.
-									8'b11100000 : out_put <= 18'b0001xx0000xxx00xx0; // Control signals for VSTW.
-									8'b11101000 : out_put <= 18'b0001xx0000xxx00xx0; // Control signals for VSTB.
+									8'b11000010 : out_put <= 17'b000000010xxx00xx1; // Control signals for VMUL.
+									8'b11000111 : out_put <= 17'b000000111xxx00xx1; // Control signals for VSR.
+									8'b11000101 : out_put <= 17'b000000101xxx00xx1; // Control signals for VSUB.
+									8'b11110000	: out_put <= 17'b000001101xxx00xx1; // Control signals for VCSUB.
+									8'b11010000	: out_put <= 17'b000000000xxx00xx1; // Control signals for VLDW.
+									8'b11011000 : out_put <= 17'b000000000xxx00xx1; // Control signals for VLDH.
+									8'b11100000 : out_put <= 17'b0001x0000xxx00xx0; // Control signals for VSTW.
+									8'b11101000 : out_put <= 17'b0001x0000xxx00xx0; // Control signals for VSTB.
 									
-									default		: out_put <= 18'bxxxxxxxxxxxxxxxxxx;
+									default		: out_put <= 17'bxxxxxxxxxxxxxxxxx;
 								endcase
 							end
 endmodule
