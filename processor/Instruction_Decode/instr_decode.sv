dbd4b2af713c1b2bb5edadbeebfd326410f05ebd
Edjchg <edjchg@gmail.com>
2020-07-11 15:44:59 -0600

    Instruction Decode. V1.1. Without decode_reg.
    Some extend modules do not belong to this stage anymore.

--- processor/Instruction_Decode/instr_decode.sv
+++ processor/Instruction_Decode/instr_decode.sv
@@ -3,47 +3,39 @@ module instr_decode(input logic clk, rst, WRITEREGISTER_WB,  // WRITEREGISTER is
 							input logic [31:0] RD_WB, // RD is the same as rd but the first one comes from Write Back.
 							input logic [255:0] INPUTDATA, // INPUTDATA is the same as InputData, but it comes from Write Back.
 							input logic [31:0] instruction,
+							//---Control Unit outputs---//
 							output logic [1:0] JMPSel,
-							output logic [31:0] Address,
-							output logic WIRTEREGISTER,
-							output logic MEMRITE,
-							output logic [1:0] REGWRITE,VCSUB,
-							output logic [2:0] ALUOP,
-							output logic [1:0] SELECTOROPB,
-							output logic SELECTORRS2,
-							output logic [1:0] BRANCHSEL,
-							output logic [1:0] SELECTOROPA,
-							output logic SELWRITEDATA,
-							output logic WRITEREGISTERVEC,
-							output logic [31:0] RS1, RS2,
-							output logic [4:0] INSTRRS1, INSTRRS2,
-							output logic [255:0] VRS1, VRS2,
-							output logic [31:0] EXTENDERRS1, EXTENDERRS2,
-							output logic [4:0] RD);
+							output logic WriteRegister,
+							output logic MemWrite,
+							output logic RegWrite,
+							output logic [1:0] vcsub,
+							output logic [2:0] ALUOp,
+							output logic [1:0] SelectorOpB,
+							output logic [1:0] BranchSel,
+							output logic [1:0]SelectorOpA,
+							output logic SelWriteData,
+							output logic WriteRegisterVec,
+							//---Registers and imm output---//
+							output logic [31:0] RSout1,RSout2,
+							output logic [31:0]ImmSignExtended,
+							output logic [255:0] VSout1, VSout2,
+							output logic [31:0] Address);
 
-logic [31:0] RSout1,RSout2;
-logic [255:0] VSout1, VSout2;
-logic Rs2;
-logic [31:0] HalfLoadValue, HalfStoreValue;
-logic [31:0]ImmSignExtended;
-logic WriteRegister;
-logic MemWrite;
-logic [1:0]RegWrite;
-logic [1:0] vcsub;
-logic [2:0] ALUOp;
-logic [1:0] SelectorOpB;
-logic SelectorRs2;
-logic [1:0] BranchSel;
-logic [1:0]SelectorOpA;
-logic SelWriteData;
-logic WriteRegisterVec;
+
+logic [4:0]Rs2;
+logic SelectorRs2; // This signal comes from Control Unit.
+
+
+
+
 
 //						OPcode				ALUop
 Control_Unit CU (instruction[31:27],instruction[11:9],JMPSel,WriteRegister,MemWrite,RegWrite,vcsub,ALUOp,SelectorOpB,SelectorRs2,BranchSel,SelectorOpA,SelWriteData,WriteRegisterVec);
 
+//									 .Rs2 from instruction.
 mux2Decode M2D (SelectorRs2,instruction[16:12],Rs2);
 
-//														  Rs1  								    // it has to be specified because INPUTDATA is 256 bus, and in this case we just use the last 32 bits.            
+//														  Rs1  								    // Number of bits has to be specified because INPUTDATA is 256 bus, and in this case we just use the last 32 bits.            
 scalar_registers SR (WRITEREGISTER_WB, clk, instruction[21:17], Rs2, RD_WB, INPUTDATA[31:0],RSout1,RSout2);
 
 //						 Imm
@@ -55,11 +47,7 @@ top_vectorial_reg VR (WRITEREGISTERVEC_WB, clk, instruction[21:17], instruction[
 //										This signal goes backwards fetch.
 extend5 E5 (instruction[26:0],Address);
 
-extend16 E16HalfLoad (RSout1[15:0],HalfLoadValue);
 
-extend16 E16HalfStore (RSout2[15:0], HalfStoreValue);
-//																																																					 Rs1[4:0]            Rs2[4:0]																			 rd[4:0]					Outputs->													
-Decode_Reg DR (clk, rst,WriteRegister,MemWrite,RegWrite,vcsub,ALUOp,SelectorOpB,SelectorRs2,BranchSel,SelectorOpA,SelWriteData,WriteRegisterVec, RSout1, RSout2,instruction[21:17], instruction[16:12],VSout1, VSout2,HalfLoadValue,HalfStoreValue, instruction[26:22], WIRTEREGISTER,MEMRITE,REGWRITE,VCSUB,ALUOP,SELECTOROPB,SELECTORRS2,BRANCHSEL,SELECTOROPA,SELWRITEDATA,WRITEREGISTERVEC,RS1, RS2,INSTRRS1, INSTRRS2,VRS1, VRS2,EXTENDERRS1, EXTENDERRS2,RD);
 
 
 
