ed082c4994196d00ceb9806d29ca3ca16ef6c1f8
Jasson Rodriguez <jassonrm@icloud.com>
2020-07-14 00:42:33 -0600

    Fix control unit ALUop

--- processor/control/Control_Unit.sv
+++ processor/control/Control_Unit.sv
@@ -19,36 +19,26 @@ module Control_Unit (input logic [4:0] OPcode,
 							assign {JMPSel, WriteRegister, MemWrite, RegWrite, vcsub, ALUOp, SelectorOpB, SelectorRs2, BranchSel,SelectorOpA,SelWriteData,WriteRegisterVec} = out_put;
 							
 							always @(*) begin
-								case ({OPcode,ALUop})       //   1 23456  7 89 A BC  -> Those numbers match with the Data order specified before.
-								   
-									8'b00000000 : out_put <= 18'b001010000000000000; // Control signals for ADD.
-									8'b01000000 : out_put <= 18'b001010000010000000; // Control signals for ADD Inmediate.
-									8'b00000001 : out_put <= 18'b001010001000000000; // Control signals for SUB.
-									8'b01001000 : out_put <= 18'b001010001010000000; // Control signals for SUB Inmediate.
-									8'b00000010 : out_put <= 18'b001010010000000000; // Control signals for MUL.
-									8'b00000100 : out_put <= 18'b001010100000000000; // Control signals for Shift Left.
-									8'b00000101 : out_put <= 18'b001010101000000000; // Control signals for AND.
-									8'b00000110 : out_put <= 18'b001010110000000000; // Control signals for Shift Right.
-									
-									8'b00010000 : out_put <= 18'b001000000101000000; // Control signals for LoadWord.
-									8'b00011000 : out_put <= 18'b001000000101000100; // Control signals for LoadHalfWord.
-									8'b00100000 : out_put <= 18'b0001x0000100001000; // Control signals for StoreWord.
-									8'b00101000 : out_put <= 18'b0001x0000100001010; // Control signals for StoreHalfWord.
-									
-									8'b10000000 : out_put <= 18'b01xxxxxxxxxx00xxx0; // Control signals for JMP.
-									8'b00110000 : out_put <= 18'b10xxxxxxxxxx00xxx0; // Control signals for JR.
-									8'b10001000 : out_put <= 18'b00xxxxxxxxxx00xxx0; // Control signals for CALL (Check it after).
-									8'b01010000 : out_put <= 18'b00xxxx0010000100x0; // Control signals for BE.
-									8'b01011000 : out_put <= 18'b00xxxx0010001000x0; // Control signals for BGT.
-									
-									8'b11000010 : out_put <= 18'b000000010xxx00xx11; // Control signals for VMUL.
-									8'b11000111 : out_put <= 18'b000000111xxx00xx11; // Control signals for VSR.
-									8'b11000101 : out_put <= 18'b000000101xxx00xx11; // Control signals for VSUB.
-									8'b11110000	: out_put <= 18'b000001101xxx00xx11; // Control signals for VCSUB.
-									8'b11010000	: out_put <= 18'b000000000xxx00xx11; // Control signals for VLDW.
-									8'b11011000 : out_put <= 18'b000000000xxx00xx11; // Control signals for VLDH.
-									8'b11100000 : out_put <= 18'b0001x0000xxx00xx00; // Control signals for VSTW.
-									8'b11101000 : out_put <= 18'b0001x0000xxx00xx00; // Control signals for VSTB.
+								case (OPcode)            //  1 23456  7 89 A BC  -> Those numbers match with the Data order specified before.
+									5'b00000 : out_put <= {6'b001010, ALUop, 9'b000000000}; // Control signals for ADD, SUB, MUL, SL, AND, SR.
+									5'b01000 : out_put <= 18'b001010000010000000; // Control signals for ADD Inmediate.
+									5'b01000 : out_put <= 18'b001010000010000000; // Control signals for ADD Inmediate.
+									5'b01001 : out_put <= 18'b001010001010000000; // Control signals for SUB Inmediate.
+									5'b00010 : out_put <= 18'b001000000101000000; // Control signals for LoadWord.
+									5'b00011 : out_put <= 18'b001000000101000100; // Control signals for LoadHalfWord.
+									5'b00100 : out_put <= 18'b0001x0000100001000; // Control signals for StoreWord.
+									5'b00101 : out_put <= 18'b0001x0000100001010; // Control signals for StoreHalfWord.
+									5'b10000 : out_put <= 18'b01xxxxxxxxxx00xxx0; // Control signals for JMP.
+									5'b00110 : out_put <= 18'b10xxxxxxxxxx00xxx0; // Control signals for JR.
+									5'b10001 : out_put <= 18'b00xxxxxxxxxx00xxx0; // Control signals for CALL (Check it after).
+									5'b01010 : out_put <= 18'b00xxxx0010000100x0; // Control signals for BE.
+									5'b01011 : out_put <= 18'b00xxxx0010001000x0; // Control signals for BGT.
+									5'b11000 : out_put <= {6'b000000, ALUop, 9'bxxx00xx11}; // Control signals for VMUL, VSR, VSUB.
+									5'b11110	: out_put <= 18'b000001101xxx00xx11; // Control signals for VCSUB.
+									5'b11010	: out_put <= 18'b000000000xxx00xx11; // Control signals for VLDW.
+									5'b11011 : out_put <= 18'b000000000xxx00xx11; // Control signals for VLDH.
+									5'b11100 : out_put <= 18'b0001x0000xxx00xx00; // Control signals for VSTW.
+									5'b11101 : out_put <= 18'b0001x0000xxx00xx00; // Control signals for VSTB.
 									
 									default		: out_put <= 18'bxxxxxxxxxxxxxxxxxx;
 								endcase
