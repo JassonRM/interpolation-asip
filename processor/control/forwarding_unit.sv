3b5a2a0a64e1c5d101ba5d376175fe2301bd8585
Jasson Rodriguez <jassonrm@icloud.com>
2020-07-13 21:39:10 -0600

    Forward 2 registers

--- processor/control/forwarding_unit.sv
+++ processor/control/forwarding_unit.sv
@@ -2,66 +2,88 @@ module forwarding_unit(input logic wb_sel, write_enable_mem, write_enable_wb,
 								input logic [4:0] rs1_ex, rs2_ex, rd_mem, rd_wb,
 								input logic[255:0] result_mem, result_wb,
 								output logic stall, OpAForward, OpBForward,
-								output logic[31:0] result,
-								output logic[255:0] vresult);
+								output logic[31:0] resultA, resultB,
+								output logic[255:0] vresultA, vresultB);
+
+logic OpAForwardMEM, OpAForwardWB, OpBForwardMEM, OpBForwardWB;
+
+assign OpAForward = OpAForwardMEM | OpAForwardWB;
+assign OpBForward = OpBForwardMEM | OpBForwardWB;
+
 								
 always_comb
-	if(write_enable_mem & (rd_mem == rs1_ex))
-		if(wb_sel)
-			begin
-				result = result_mem[31:0];
-				vresult = result_mem;
-				OpAForward = 1;
-				OpBForward = 0;
-				stall = 0;
-			end
-		else
-			begin
-				result = 'z;
-				vresult = 'z;
-				OpAForward = 0;
-				OpBForward = 0;
-				stall = 1;
-			end
-	else if(write_enable_mem & wb_sel & (rd_mem == rs2_ex))
-		if(wb_sel)
-			begin
-				result = result_mem[31:0];
-				vresult = result_mem;
-				OpAForward = 0;
-				OpBForward = 1;
-				stall = 0;
-			end
-		else
-			begin
-				result = 'z;
-				vresult = 'z;
-				OpAForward = 0;
-				OpBForward = 0;
-				stall = 1;
-			end
-	else if(write_enable_wb & (rd_wb == rs1_ex))
+	begin
+	if(write_enable_mem & wb_sel & (rd_mem == rs1_ex))
 		begin
-			result = result_wb[31:0];
-			vresult = result_wb;
-			OpAForward = 1;
-			OpBForward = 0;
+			OpAForwardMEM = 1;
+			OpBForwardMEM = 0;
 			stall = 0;
 		end
-	else if(write_enable_wb & (rd_wb == rs2_ex))
+	else if(write_enable_mem & wb_sel & (rd_mem == rs2_ex))
 		begin
-			result = result_wb[31:0];
-			vresult = result_wb;
-			OpAForward = 0;
-			OpBForward = 1;
+			OpAForwardMEM = 0;
+			OpBForwardMEM = 1;
 			stall = 0;
+		end
+	else if(write_enable_mem & !wb_sel & ((rd_mem == rs1_ex) | (rd_mem == rs2_ex)))
+		begin
+			OpAForwardMEM = 0;
+			OpBForwardMEM = 0;
+			stall = 1;
 		end
 	else
 		begin
-			result = 'z;
-			vresult = 'z;
-			OpAForward = 0;
-			OpBForward = 0;
+			OpAForwardMEM = 0;
+			OpBForwardMEM = 0;
 			stall = 0;
 		end
+		
+	if(write_enable_wb & (rd_wb == rs1_ex))
+		begin
+			OpAForwardWB = 1;
+			OpBForwardWB = 0;
+		end
+	else if(write_enable_wb & (rd_wb == rs2_ex))
+		begin
+			OpAForwardWB = 0;
+			OpBForwardWB = 1;
+		end
+	else
+		begin
+			OpAForwardWB = 0;
+			OpBForwardWB = 0;
+		end
+	
+	if(OpAForwardMEM)
+		begin
+			resultA = result_mem[31:0];
+			vresultA = result_mem;
+		end
+	else if(OpAForwardWB)
+		begin
+			resultA = result_wb[31:0];
+			vresultA = result_wb;
+		end
+	else
+		begin
+			resultA = 'z;
+			vresultA = 'z;
+		end
+		
+	if(OpBForwardMEM)
+		begin
+			resultB = result_mem[31:0];
+			vresultB = result_mem;
+		end
+	else if(OpBForwardWB)
+		begin
+			resultB = result_wb[31:0];
+			vresultB = result_wb;
+		end
+	else
+		begin
+			resultB = 'z;
+			vresultB = 'z;
+		end
+	end
 endmodule
--- processor/control/forwarding_unit_test.sv
+++ processor/control/forwarding_unit_test.sv
@@ -4,16 +4,17 @@ logic wb_sel, write_enable_mem, write_enable_wb;
 logic [4:0] rs1_ex, rs2_ex, rd_mem, rd_wb;
 logic[255:0] result_mem, result_wb;
 logic stall, OpAForward, OpBForward;
-logic[31:0] result;
-logic[255:0] vresult;
+logic[31:0] resultA, resultB;
+logic[255:0] vresultA, vresultB;
 
-forwarding_unit DUT(wb_sel, write_enable_mem, write_enable_wb, rs1_ex, rs2_ex, rd_mem, rd_wb, result_mem, result_wb, stall, OpAForward, OpBForward, result, vresult);
+forwarding_unit DUT(wb_sel, write_enable_mem, write_enable_wb, rs1_ex, rs2_ex, rd_mem, rd_wb, result_mem, result_wb, stall, OpAForward, OpBForward, resultA, resultB, vresultA, vresultB);
 
 initial begin
 
 wb_sel = 1; write_enable_mem = 1; write_enable_wb = 1; rs1_ex = 'd1; rs2_ex = 'd2; rd_mem = 'd3; rd_wb = 'd4; result_mem = 'h1234567890ABCDEF; result_wb = 'hFEDCBA0987654321;
 
 #10 rs1_ex = 'd3;
+#10 rs2_ex = 'd4;
 #10 wb_sel = 0;
 #10 wb_sel = 1;
 #10 write_enable_mem = 0;
