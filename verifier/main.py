5e09bbf40e316d95887d88b1705f269c43174fc0
Marco <m.herrera0799@gmail.com>
2020-11-08 23:14:21 -0600

    Functional binomial interpolation

--- verifier/main.py
+++ verifier/main.py
@@ -3,26 +3,19 @@ import cv2
 
 
 def main():
-    with open("data.txt", "r") as input:
-        data = input.readlines()
+    input_image = cv2.imread('sample.png', 0)
+    cv2.imshow("INPUT", input_image)
+    quadrant_size = 120
 
-    imageWidth = 320
-    imageHeight = 320
-
-    encrypted = []
-    for line in data:
-        encrypted += line.split()
-
-    print(encrypted.__len__())
-    encrypted_image = numpy.array(encrypted, dtype=numpy.uint8)
-    encrypted_image.shape = (2 * imageHeight, imageWidth)
-    cv2.imshow("Encrypted", encrypted_image)
+    output = [0] * (2 * quadrant_size) ** 2
+    binomial(input_image.flatten(), input_image.shape[1], output, quadrant_size, quadrant_size)
+    output_image = numpy.array(output, dtype=numpy.uint8)
+    output_image.shape = (2 * quadrant_size, 2 * quadrant_size)
+    cv2.imshow("Other", output_image)
     cv2.waitKey()
-    output = [0] * 64
-    binomial([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], 4, output, 4, 4)
-    print(output)
 
 
+# TODO Manage all widths
 def binomial(input_img, input_width, output_img, division_width, division_height):
     column = 0
     read_addr = 0
